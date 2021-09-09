4de3f5a5d340f3940d32dc0f73154d555196c55a
Felipe dos anjos <40957376+FelipeDasr@users.noreply.github.com>
2021-09-08 21:15:41 -0300

    Add files

---
+++ Client/index-client.py
@@ -0,0 +1,45 @@
+import threading
+import socket
+
+
+def main():
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect(('localhost', 7777))
+    except:
+        return print('\nNão foi possívvel se conectar ao servidor!\n')
+
+    username = input('Usuário> ')
+    print('\nConectado')
+
+    thread1 = threading.Thread(target=receiveMessages, args=[client])
+    thread2 = threading.Thread(target=sendMessages, args=[client, username])
+
+    thread1.start()
+    thread2.start()
+
+
+def receiveMessages(client):
+    while True:
+        try:
+            msg = client.recv(2048).decode('utf-8')
+            print(msg+'\n')
+        except:
+            print('\nNão foi possível permanecer conectado no servidor!\n')
+            print('Pressione <Enter> Para continuar...')
+            client.close()
+            break
+            
+
+def sendMessages(client, username):
+    while True:
+        try:
+            msg = input('\n')
+            client.send(f'<{username}> {msg}'.encode('utf-8'))
+        except:
+            return
+
+
+main()
---
+++ Server/index-server.py
@@ -0,0 +1,45 @@
+import threading
+import socket
+
+clients = []
+
+def main():
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        server.bind(('localhost', 7777))
+        server.listen()
+    except:
+        return print('\nNão foi possível iniciar o servidor!\n')
+
+    while True:
+        client, addr = server.accept()
+        clients.append(client)
+
+        thread = threading.Thread(target=messagesTreatment, args=[client])
+        thread.start()
+
+def messagesTreatment(client):
+    while True:
+        try:
+            msg = client.recv(2048)
+            broadcast(msg, client)
+        except:
+            deleteClient(client)
+            break
+
+
+def broadcast(msg, client):
+    for clientItem in clients:
+        if clientItem != client:
+            try:
+                clientItem.send(msg)
+            except:
+                deleteClient(clientItem)
+
+
+def deleteClient(client):
+    clients.remove(client)
+
+main()
